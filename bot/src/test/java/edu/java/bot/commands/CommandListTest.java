9a524aeb69fbe9ff564f5a287a807a396ad757bb
lesam19092 <danigpro1337@gmail.com>
2024-02-15 18:46:35 +0300

    добавил тесты

--- bot/src/test/java/edu/java/bot/commands/CommandListTest.java
+++ bot/src/test/java/edu/java/bot/commands/CommandListTest.java
@@ -1,24 +1,72 @@
 package edu.java.bot.commands;
 
+import edu.java.bot.repository.UserService;
+import edu.java.bot.users.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CommandListTest {
 
+    private final String unknownUser = "Пользователь не существует. Зарегистрируйтесь с помощью команды /start";
+    private final String emptyList = "Вы не отслеживаете ни одной ссылки";
+
+    private CommandList commandList;
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        userService = new UserService();
+        commandList = new CommandList(userService);
+    }
+
     @Test
     void command() {
+        assertEquals(commandList.command(), "/list");
     }
 
     @Test
     void description() {
+        assertEquals(commandList.description(), "Позволяет получить список отслеживаемых ссылок.");
     }
 
     @Test
-    void handle() {
+    void getTrackingLinksNotRegistred() {
+        assertEquals(commandList.getTrackingLinks(-1L), unknownUser);
     }
 
     @Test
-    void getTrackingLinks() {
+    void getTrackingLinksEmptyList() {
+        User mockUser = mock(User.class);
+
+        List<URI> sites = new ArrayList<>();
+
+        when(mockUser.getSites()).thenReturn(sites);
+        when(mockUser.getId()).thenReturn(10L);
+        userService.saveUser(mockUser);
+        assertEquals(commandList.getTrackingLinks(10L), emptyList);
+
+    }
+
+    @Test
+    void getTrackingLinks() throws URISyntaxException {
+        User mockUser = mock(User.class);
+        List<URI> sites = new ArrayList<>();
+        sites.add(new URI("https://github.com"));
+
+        when(mockUser.getSites()).thenReturn(sites);
+        when(mockUser.getId()).thenReturn(10L);
+        userService.saveUser(mockUser);
+        assertEquals(commandList.getTrackingLinks(10L), "1) https://github.com\n");
+
+
     }
 }
--- bot/src/test/java/edu/java/bot/commands/CommandStartTest.java
+++ bot/src/test/java/edu/java/bot/commands/CommandStartTest.java
@@ -1,20 +1,51 @@
 package edu.java.bot.commands;
 
+import edu.java.bot.repository.UserService;
+import edu.java.bot.users.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CommandStartTest {
 
+
+    private final String unknownUser = "Пользователь не существует. Зарегистрируйтесь с помощью команды /start";
+    private final String emptyList = "Вы не отслеживаете ни одной ссылки";
+
+    private CommandStart commandStart;
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        userService = new UserService();
+        commandStart = new CommandStart(userService);
+    }
+
     @Test
     void command() {
+        assertEquals(commandStart.command(),"/start");
     }
 
     @Test
     void description() {
+        assertEquals(commandStart.description(),"Позволяет зарегистрироваться в нашей системе.");
     }
 
     @Test
     void handle() {
     }
+
+    @Test
+    void handleNotRegisted(){
+        User mockUser = mock(User.class);
+
+        when(mockUser.getId()).thenReturn(-1l);
+
+        assertEquals(commandStart.handle(null),unknownUser);
+
+    }
+
+
 }
