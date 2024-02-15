5c240c4784404380c4845774e7e4d9e83d960ef5
lesam19092 <danigpro1337@gmail.com>
2024-02-15 14:52:47 +0300

    пытаюсь все нормально настроить

--- bot/src/test/java/SampleTest.java
+++ bot/src/test/java/SampleTest.java
@@ -1,6 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 public class SampleTest {
     @Test
