271b4f20bb4438ab57a80d91114988eaf236947a
lesam19092 <danigpro1337@gmail.com>
2024-03-04 21:56:59 +0300

    подключаю бд

---
+++ mirgations/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE chat
+(
+    id      bigint NOT NULL PRIMARY KEY,
+    chat_id bigint NOT NULL,
+
+);
+
+
+CREATE TABLE link
+(
+    id              bigint generated always as identity,
+    url             text                     not null,
+    chat_id         bigint                   NOT NULL REFERENCES chat (id),
+
+    last_check_time timestamp with time zone not null,
+
+    created_at      timestamp with time zone not null,
+    created_by      text                     not null,
+
+
+    primary key (id),
+    unique (url)
+);
+
+
