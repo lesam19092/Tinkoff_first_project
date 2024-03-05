61b98580a14345a43efaaaa0721b4f47c331a8a6
lesam19092 <danigpro1337@gmail.com>
2024-03-05 23:38:27 +0300

    пытаюсь пофиксить баги

--- scrapper/src/main/java/edu.java/controller/LinksApiController.java
+++ scrapper/src/main/java/edu.java/controller/LinksApiController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import javax.annotation.Generated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,7 +35,7 @@ public class LinksApiController implements LinksApi {
 
     private final HttpServletRequest request;
 
-    @org.springframework.beans.factory.annotation.Autowired
+    @Autowired
     public LinksApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
