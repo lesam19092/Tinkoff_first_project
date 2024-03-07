697f3339029af0ae12a777a0d4bb49c38afdce77
lesam19092 <danigpro1337@gmail.com>
2024-03-07 21:49:51 +0300

    добавил тест

--- scrapper/src/test/java/edu/java/scrapper/ExampleIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/ExampleIntegrationTest.java
@@ -1,31 +1,20 @@
 package edu.java.scrapper;
 
-import com.zaxxer.hikari.HikariDataSource;
-import liquibase.database.Database;
-import liquibase.exception.LiquibaseException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import javax.sql.DataSource;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
+import liquibase.database.Database;
+import liquibase.exception.LiquibaseException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class ExampleIntegrationTest extends IntegrationTest {
 
-
-
     @Test
     public void testLiquibaseMigration() throws SQLException, FileNotFoundException, LiquibaseException {
         // Verify that Liquibase migrations have been applied successfully
-        System.out.println("dsadsada");
         Database database = IntegrationTest.runMigrations(POSTGRES);
-
-        System.out.println(database.getDisplayName());
 
         assertThat(database).isNotNull();
         // Add assertions as needed
--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,31 +1,24 @@
 package edu.java.scrapper;
 
-import com.zaxxer.hikari.HikariDataSource;
-import liquibase.Contexts;
-import liquibase.LabelExpression;
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.DatabaseException;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
-import liquibase.resource.DirectoryResourceAccessor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Connection;
-import static org.postgresql.core.ConnectionFactory.openConnection;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class IntegrationTest {
