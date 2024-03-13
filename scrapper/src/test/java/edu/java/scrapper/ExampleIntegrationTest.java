b3f3fcb75c8440af076f561c153e9a3221d18d08
lesam19092 <danigpro1337@gmail.com>
2024-03-13 14:53:37 +0300

    пытаюсь пофиксить ошибки

--- scrapper/src/test/java/edu/java/scrapper/ExampleIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/ExampleIntegrationTest.java
@@ -1,17 +1,11 @@
 package edu.java.scrapper;
 
-import com.zaxxer.hikari.HikariDataSource;
-import liquibase.database.Database;
-import liquibase.exception.LiquibaseException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import javax.sql.DataSource;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
+import liquibase.database.Database;
+import liquibase.exception.LiquibaseException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
