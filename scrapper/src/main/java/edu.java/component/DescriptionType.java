b5fe5833a490bca93d18c480dcdf16bc77df0fae
lesam19092 <danigpro1337@gmail.com>
2024-03-26 22:39:31 +0300

    checkstyle

--- scrapper/src/main/java/edu.java/component/DescriptionType.java
+++ scrapper/src/main/java/edu.java/component/DescriptionType.java
@@ -10,10 +10,12 @@ public enum DescriptionType {
     public static String getDescription(List<DescriptionType> list) {
         StringBuilder str = new StringBuilder();
         for (DescriptionType type : list) {
-            switch (type) {
-                case UPDATING_DATA -> str.append("обновление данных : ");
-                case NEW_COMMENT -> str.append("\n" + "появился новый ответ");
-                case NEW_ANSWER -> str.append("\n" + "появился новый комментарий");
+            if (type == UPDATING_DATA) {
+                str.append("обновление данных : ");
+            } else if (type == NEW_COMMENT) {
+                str.append("\n" + "появился новый ответ");
+            } else if (type == NEW_ANSWER) {
+                str.append("\n" + "появился новый комментарий");
             }
         }
         return str.toString();
