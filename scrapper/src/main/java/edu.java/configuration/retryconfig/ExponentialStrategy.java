58d777207401541a470f22ead3da126504a63b02
lesam19092 <danigpro1337@gmail.com>
2024-03-31 22:09:37 +0300

    добавил бины для опредения backoff

--- scrapper/src/main/java/edu.java/configuration/retryconfig/ExponentialStrategy.java
+++ scrapper/src/main/java/edu.java/configuration/retryconfig/ExponentialStrategy.java
@@ -1,12 +1,14 @@
 package edu.java.configuration.retryconfig;
 
+import java.time.Duration;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.util.retry.Retry;
 import reactor.util.retry.RetryBackoffSpec;
-import java.time.Duration;
 
 @Configuration
 @ConditionalOnProperty(prefix = "app", name = "retry-strategy", havingValue = "exponential")
@@ -17,8 +19,12 @@ public class ExponentialStrategy {
     @Value("${app.retry-delay}")
     private int delay;
 
-   /* @Bean
+    @Autowired
+    private List<RuntimeException> retryOnExceptions;
+
+    @Bean
     public RetryBackoffSpec retryBackoffSpec() {
-        return Retry.backoff(maxAttempts, Duration.ofSeconds(delay));
-    }*/
+        return Retry.backoff(maxAttempts, Duration.ofSeconds(delay)).filter(throwable -> retryOnExceptions.stream()
+            .anyMatch(x -> x.getClass().isAssignableFrom(throwable.getClass())));
+    }
 }
--- scrapper/src/main/java/edu.java/configuration/retryconfig/LinearStrategy.java
+++ scrapper/src/main/java/edu.java/configuration/retryconfig/LinearStrategy.java
@@ -1,18 +1,15 @@
 package edu.java.configuration.retryconfig;
 
-import java.time.Duration;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.util.retry.Retry;
-import reactor.util.retry.RetryBackoffSpec;
 
 @Configuration
-@ConditionalOnProperty(prefix = "app", name = "retry-strategy", havingValue = "const")
-public class ConstantStrategy {
+@ConditionalOnProperty(prefix = "app", name = "retry-strategy", havingValue = "linear")
+public class LinearStrategy {
+    //TODO implement linear
     @Value("${app.retry-max-attempts}")
     private int maxAttempts;
 
@@ -21,10 +18,4 @@ public class ConstantStrategy {
 
     @Autowired
     private List<RuntimeException> retryOnExceptions;
-
-    @Bean
-    public RetryBackoffSpec retryBackoffSpec() {
-        return Retry.fixedDelay(maxAttempts, Duration.ofSeconds(delay)).filter(throwable -> retryOnExceptions.stream()
-            .anyMatch(x -> x.getClass().isAssignableFrom(throwable.getClass())));
-    }
 }
