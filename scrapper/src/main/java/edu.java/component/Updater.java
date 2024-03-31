6fa310972e8363a1c65ab871e1faae191cd6e6b7
lesam19092 <danigpro1337@gmail.com>
2024-03-31 22:15:42 +0300

    добавил retry for github и обработал ошибки в updater

--- scrapper/src/main/java/edu.java/component/Updater.java
+++ scrapper/src/main/java/edu.java/component/Updater.java
@@ -1,6 +1,8 @@
 package edu.java.component;
 
 import edu.java.client.BotClient;
+import edu.java.exception.ClientException;
+import edu.java.exception.ServerException;
 import edu.java.github.GitHubClient;
 import edu.java.github.GitHubRepository;
 import edu.java.model.dto.Link;
@@ -12,38 +14,48 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.jboss.logging.Logger;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 
 @Component
 public class Updater implements LinkUpdater {
+    private final Logger logger = Logger.getLogger(Updater.class.getName());
 
     private final LinkRepository linkRepository;
     private final GitHubClient gitHubClient;
     private final StackOverFlowClient stackOverFlowClient;
-    private final BotClient botClient = new BotClient(WebClient.builder().build());
+    private final BotClient botClient;
 
     public Updater(
         LinkRepository linkRepository,
         GitHubClient gitHubClient,
-        StackOverFlowClient stackOverFlowClient
+        StackOverFlowClient stackOverFlowClient,
+        BotClient botClient
     ) {
         this.linkRepository = linkRepository;
         this.gitHubClient = gitHubClient;
         this.stackOverFlowClient = stackOverFlowClient;
+        this.botClient = botClient;
     }
 
     @Override
     public void updateLinkForGithub(Link link) throws URISyntaxException {
-        Timestamp now = Timestamp.valueOf(LocalDateTime.now());
-        List<String> fragments = List.of(link.getUrl().toString().split("/"));
-        GitHubRepository rep = gitHubClient
-            .getRepositoryInfo(fragments.get(Integer.parseInt("3")), fragments.get(Integer.parseInt("4")))
-            .block();
-        Timestamp lastPush = rep.getLastPush();
-        if (lastPush.after(link.getLastCheckTime())) {
-            linkRepository.updateLinkLastCheckTimeById(link.getId(), now);
-            botClient.updateLink(link.getUrl(), List.of(link.getChatId()), "обновление данных");
+
+        try {
+
+            Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+            List<String> fragments = List.of(link.getUrl().toString().split("/"));
+            GitHubRepository rep = gitHubClient
+                .getRepositoryInfo(fragments.get(Integer.parseInt("3")), fragments.get(Integer.parseInt("4")))
+                .block();
+            Timestamp lastPush = rep.getLastPush();
+            if (lastPush.after(link.getLastCheckTime())) {
+                linkRepository.updateLinkLastCheckTimeById(link.getId(), now);
+                botClient.updateLink(link.getUrl(), List.of(link.getChatId()), "обновление данных");
+            }
+        } catch (ServerException | ClientException | WebClientRequestException ex) {
+            logger.error(ex.getMessage());
         }
     }
 
@@ -60,23 +72,30 @@ public class Updater implements LinkUpdater {
     }
 
     private void getUpdatesFromSof(Link link, StackOverFlowQuestion question) throws URISyntaxException {
-        Timestamp now = Timestamp.valueOf(LocalDateTime.now());
-        Timestamp lastActivity = question.getLastActivityAsTimestamp();
-        if (lastActivity.after(link.getLastCheckTime())) {
-            List<DescriptionType> lisOfDescriptions = new ArrayList<>();
-            lisOfDescriptions.add(DescriptionType.UPDATING_DATA);
-            linkRepository.updateLinkLastCheckTimeById(link.getId(), now);
-            if (question.getAnswerCount() > linkRepository.getLinkPropertiesById(link.getId()).getCountOfAnswer()) {
-                lisOfDescriptions.add(DescriptionType.NEW_COMMENT);
-                linkRepository.updateCountOfAnswersById(link.getId(), question.getAnswerCount());
-            }
-            if (question.getCommentCount() > linkRepository.getLinkPropertiesById(link.getId()).getCountOfComments()) {
-                lisOfDescriptions.add(DescriptionType.NEW_ANSWER);
-                linkRepository.updateCountOfCommentsById(link.getId(), question.getCommentCount());
-            }
-            String description = DescriptionType.getDescription(lisOfDescriptions);
-            botClient.updateLink(link.getUrl(), List.of(link.getChatId()), description);
 
+        try {
+
+            Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+            Timestamp lastActivity = question.getLastActivityAsTimestamp();
+            if (lastActivity.after(link.getLastCheckTime())) {
+                List<DescriptionType> lisOfDescriptions = new ArrayList<>();
+                lisOfDescriptions.add(DescriptionType.UPDATING_DATA);
+                linkRepository.updateLinkLastCheckTimeById(link.getId(), now);
+                if (question.getAnswerCount() > linkRepository.getLinkPropertiesById(link.getId()).getCountOfAnswer()) {
+                    lisOfDescriptions.add(DescriptionType.NEW_COMMENT);
+                    linkRepository.updateCountOfAnswersById(link.getId(), question.getAnswerCount());
+                }
+                if (question.getCommentCount()
+                    > linkRepository.getLinkPropertiesById(link.getId()).getCountOfComments()) {
+                    lisOfDescriptions.add(DescriptionType.NEW_ANSWER);
+                    linkRepository.updateCountOfCommentsById(link.getId(), question.getCommentCount());
+                }
+                String description = DescriptionType.getDescription(lisOfDescriptions);
+                botClient.updateLink(link.getUrl(), List.of(link.getChatId()), description);
+
+            }
+        } catch (ClientException | ServerException e) {
+            logger.error(e.getMessage());
         }
     }
 }
