3b15f441dcb0e1d793876fc842df39f06876c64b
lesam19092 <danigpro1337@gmail.com>
2024-04-19 18:27:15 +0300

    some refactoring

--- bot/src/main/java/edu/java/bot/controllers/UpdatesApiController.java
+++ bot/src/main/java/edu/java/bot/controllers/UpdatesApiController.java
@@ -1,10 +1,13 @@
 package edu.java.bot.controllers;
 
-import edu.java.bot.model.request.LinkUpdateRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.request.LinkUpdateRequest;
 import edu.java.bot.service.MessageService;
+import edu.java.bot.service.MessageServiceInterface;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,12 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UpdatesApiController implements UpdatesApi {
+    private final ObjectMapper objectMapper;
 
-    private final MessageService messageService;
+    private final HttpServletRequest request;
+
+    private final MessageServiceInterface messageService;
 
     @Autowired
-    public UpdatesApiController(MessageService messageService) {
-
+    public UpdatesApiController(ObjectMapper objectMapper, HttpServletRequest request, MessageService messageService) {
+        this.objectMapper = objectMapper;
+        this.request = request;
         this.messageService = messageService;
     }
 
@@ -27,7 +34,9 @@ public class UpdatesApiController implements UpdatesApi {
         @Parameter(in = ParameterIn.DEFAULT, description = "", required = true, schema = @Schema()) @Valid
         @RequestBody LinkUpdateRequest body
     ) {
-        messageService.sendNotification(body.getTgChatIds(), body.getUrl(), body.getDescription());
+        messageService.sendNotification(body);
+
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
+
 }
